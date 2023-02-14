cf5d88881e9420504f2ea5fa9f40c50d9a5ffa61
Vinit <vinit224488@gmail.com>
2023-02-14 11:24:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+// import './App.css';
+// import Login from './pages/Login';
+// import Canvastools from './components/Canvastools';
+// import CaseBlog from './pages/CaseBlog';
+// import Home from './pages/Home'
+
+import ForgetPassword from "./pages/forgetpasswordPage";
+// import NewCase from "./pages/NewCase";
+
+function App() {
+  return (
+    // <Login/>
+    // <Home/>
+    // <CaseBlog />
+    // <Canvastools />
+    // <NewCase />
+    <ForgetPassword/>
+  );
+}
+
+export default App;
