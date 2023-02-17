bff6959feabb595fede37ec0212c545c6cfe54a3
mayur <shelkemayur4700@gmail.com>
2023-02-17 12:42:34 +0530

    added footer and toolbar

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@
 // import Canvastools from './components/Canvastools';
 // import CaseBlog from './pages/CaseBlog';
 // import Home from './pages/Home'
-
-import ForgetPassword from "./pages/forgetpasswordPage";
-// import NewCase from "./pages/NewCase";
-
+// import ForgetPassword from "./pages/forgetpasswordPage";
+import NewCase from "./pages/NewCase";
+import { Route, Routes, } from "react-router-dom";
+// import CaseBlog from "./pages/CaseBlog";
+import Footer from './components/Footer';
 function App() {
   return (
     // <Login/>
     // <Home/>
     // <CaseBlog />
     // <Canvastools />
-    // <NewCase />
-    <ForgetPassword/>
-  );
+    <>
+    <NewCase />
+    {/* <Footer/> */}
+    {/* <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/caseBlog" element={<CaseBlog/>}/>
+    </Routes>
+    <ForgetPassword/> */}
+    </>
+  )
 }
 
 export default App;
